1f4bbff8e70968ef252e473a8a2d5c1a0888bff5
Olaleye Praise <65808506+Mpraizcool@users.noreply.github.com>
2024-12-10 13:28:22 +0100

    Introduction

---
+++ laravel.php
@@ -0,0 +1,10 @@
+<!-- resources/views/example.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Example</title>
+</head>
+<body>
+    <h1>Welcome, {{ $name }}!</h1>
+</body>
+</html>
